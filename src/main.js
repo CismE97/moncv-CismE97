03236b836c819c83e688ec75d3a4ff0f091a9b79
Christe Thibaud <thibaudchriste1997@gmail.com>
2017-10-25 11:02:31 +0200

    Ajout de jquery-smooth-scroll

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import 'bootstrap.native';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './main.css';
+import 'jquery-smooth-scroll';
+
+$(document).ready(() => {
+    $('a').smoothScroll();
+});
