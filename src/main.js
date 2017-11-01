ebad2c9f6ad844eeed22d56a798fb4e39377aa1f
Christe Thibaud <thibaudchriste1997@gmail.com>
2017-11-01 10:54:12 +0100

    Création des progress bar avec ChartJS

--- src/main.js
+++ src/main.js
@@ -6,5 +6,24 @@ import 'jquery-smooth-scroll';
 import Chart from 'chart.js';
 
 $(document).ready(() => {
-    $('a').smoothScroll(); 
+    $('a').smoothScroll();
+    $('.progress').each((index, element) => {
+        let value = $(element.children).attr('aria-valuenow');
+        let remainingValue = $(element.children).attr('aria-valuemax') - value;
+
+        $(element).replaceWith('<canvas id="bar-chart-' + index + '" width="100" height="100"></canvas>');
+        let ctx = $('#bar-chart-' + index + '');
+        // eslint-disable-next-line
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [
+                    {
+                        backgroundColor: ['#F5484A', '#EEEEF2'],
+                        data: [value, remainingValue]
+                    }
+                ]
+            }
+        });
+    });
 });
