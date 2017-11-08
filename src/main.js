006355cdd59485fadb81f98f73f738674b30f70e
Christe Thibaud <thibaudchriste1997@gmail.com>
2017-11-08 08:33:52 +0100

    Ajout de la libraire scrollreveal pour faire apparaître des elements du Dom au scroll de la page + correction code Chart.js (mise d'une seule recherche dans le dom au lieu de 2)

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import 'bootstrap.native';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './main.css';
-import 'jquery-smooth-scroll';
 
+import 'jquery-smooth-scroll';
+import ScrollReveal from 'scrollreveal';
 import Chart from 'chart.js';
 
 $(document).ready(() => {
     $('a').smoothScroll();
+
+    // Création des progress bar.
     $('.progress').each((index, element) => {
         let value = $(element.children).attr('aria-valuenow');
         let remainingValue = $(element.children).attr('aria-valuemax') - value;
 
-        $(element).replaceWith('<canvas id="bar-chart-' + index + '" width="100" height="100"></canvas>');
-        let ctx = $('#bar-chart-' + index + '');
+        const canevas = $('<canvas id="bar-chart-' + index + '" width="100" height="100"></canvas>');
+        $(element).replaceWith(canevas);
         // eslint-disable-next-line
-        new Chart(ctx, {
+        new Chart(canevas, {
             type: 'doughnut',
             data: {
                 datasets: [
@@ -31,4 +34,10 @@ $(document).ready(() => {
             }
         });
     });
+
+     // eslint-disable-next-line
+    let element = new ScrollReveal();
+    element.reveal('h2');
+    element.reveal('.skill');
+    element.reveal('.media');
 });
